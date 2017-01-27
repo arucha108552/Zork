9281e4b2bee1c6b4ef51521d2bc0c9ff2df45370
Thienthai <thienthai@Thienthais-MacBook-Pro.local>
2017-01-27 23:27:35 +0700

    thienthai commit

---
+++ readme-git.txt
@@ -0,0 +1,8 @@
+# ignore all .jpg in the directory because .jpg is not the part of my work
+*.png
+# ignore the backup file because I want to backup the original file and don't want
+# git to track
+backup
+# ignore the prototype file incase i want to test the game first and don't want git
+# to track it
+prototype
--- src/main/java/io/muic/ooc/thienthai/zork/startGame.java
+++ src/main/java/io/muic/ooc/thienthai/zork/startGame.java
@@ -1,13 +1,50 @@
 package io.muic.ooc.thienthai.zork;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
  */
+
+class Map{
+
+    private int levelCleared = 0;
+
+    public Map(){
+
+    }
+
+    public String[] getMap(){
+
+        String[] map1 = {
+
+                "#########################################",
+                "#C        =I                           I#",
+                "#         =             M               #",
+                "#         =    M                 ==DDD==#",
+                "====DDD====               M      =      #",
+                "#                  I             =C     #",
+                "#     ###################################",
+                "#S    ###################################",
+                "#########################################",
+
+        };
+
+        return map1;
+    }
+
+}
+
 public class startGame
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "The Game is Start" );
+//        Map map = new Map();
+//        System.out.println(map.getMap()[1].charAt(1));
+        BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
     }
 }
