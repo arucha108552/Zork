2b353ad8a51f9c3d0a9e0be73c4c03f49a353401
Thienthai <thienthai@Thienthais-MacBook-Pro.local>
2017-01-27 17:50:39 +0700

    thienthai commit

---
+++ src/main/java/io/muic/ooc/thienthai/zork/startGame.java
@@ -0,0 +1,13 @@
+package io.muic.ooc.thienthai.zork;
+
+/**
+ * Hello world!
+ *
+ */
+public class startGame
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "The Game is Start" );
+    }
+}
---
+++ src/test/java/io/muic/ooc/thienthai/zork/startGameTest.java
@@ -0,0 +1,38 @@
+package io.muic.ooc.thienthai.zork;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple startGame.
+ */
+public class startGameTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public startGameTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( startGameTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
